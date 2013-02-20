ad61e5fad24cff7b61d6ef1f08a3a8f7b1f56edc
Sean <ksean@SeanNoteBook.(none)>
2013-02-19 23:56:43 -0600

    modified:   GDMUtils.py

--- pysrc/gdmutil/GDMUtils.py
+++ pysrc/gdmutil/GDMUtils.py
@@ -10,7 +10,6 @@ class GDMUtils:
 
   #formula off of cmegroup.com website
   def calc_cme_hv_annual(self, data):
-
     trading_days_factor = math.sqrt(252)
     list_of_ln_returns = list()
     sorted_keys = sorted(data.iterkeys())
@@ -24,6 +23,31 @@ class GDMUtils:
     cme_volatility = np.std(list_of_ln_returns) * trading_days_factor
     print "GDMUntils::calc_cme_histvol cme_volatility:" + str(cme_volatility)
     return cme_volatility
+
+  #calcs the annualized historical vol for the specified number of days
+  def calc_cme_hv_annual2(self, data, days=30):
+    trading_days_factor = math.sqrt(252)
+    ln_of_returns = list()
+    sorted_keys = sorted(data.iterkeys())
+    prior_settle_px = data[sorted_keys[0]]
+    vols = dict()
+    curr_idx = 0
+    trailing_idx = 0
+
+    for key in sorted_keys[1:]:
+      settle_px = data[key]
+      ln_of_returns.append(math.log(settle_px/prior_settle_px))
+
+      #set variables
+      if(len(ln_of_returns) >= days):
+        vol = np.std(ln_of_returns[trailing_idx:curr_idx])*trading_days_factor
+        vols[key] = vol
+        curr_idx += 1
+      curr_idx += 1
+      prior_settle_px = settle_px
+
+    return vols
+
 
   def calc_cme_hv_daily(self, data, days):
     trading_days_factor = math.sqrt(252)
